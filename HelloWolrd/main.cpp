51e97209ea697c67046b1397870dd8bba5c345e8
Max Gao <gaoxt9@hotmail.com>
2017-10-03 23:34:24 -0700

    Hello World!

---
+++ HelloWolrd/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+	cout << "This is like taken me forever!" << endl;
+	system("pause");
+	return 0;
+}
